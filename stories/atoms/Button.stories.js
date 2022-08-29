084b973e877c159600966a016c6d41fb449d775f
geovannygil <geovannygr2018@gmail.com>
2022-08-28 19:41:11 -0600

    Init project

---
+++ stories/atoms/Button.stories.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from '../../atomic/atoms/Button'
+
+
+
+export default {
+  // title: 'Atoms/Button', 
+  component: Button
+}
+
+const Template = (args) => <Button {...args} />
+
+export const Primary = Template.bind({})
+Primary.args = {
+  children: 'Primary'
+}
